d5539f0d31acf2951dd7fb9e4a6cdde41a23a698
FAZLE RAKIB <fazle30@student.sust.edu>
2021-01-07 22:09:05 +0600

    Added gitignore file

--- backend-server/db/config.js
+++ backend-server/db/config.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('node_complete','root','Pass',{
+const sequelize = new Sequelize('node_complete','root','',{
     dialect:'mysql',
     host:'localhost'
   });
