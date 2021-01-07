7ed25fd44058062f72de8ccc220d5d1b276bc326
FAZLE RAKIB <fazle30@student.sust.edu>
2021-01-07 21:51:11 +0600

    Minor Change

--- backend-server/db/config.js
+++ backend-server/db/config.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('node_complete','root','',{
+const sequelize = new Sequelize('node_complete','root','Password',{
     dialect:'mysql',
     host:'localhost'
   });
