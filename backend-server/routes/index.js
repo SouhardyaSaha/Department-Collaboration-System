01ae2eaae1bde7945490e65afcc7a8d6ed08d175
Souhardya Saha <43247008+SouhardyaSaha@users.noreply.github.com>
2021-01-10 15:51:06 +0600

    Routes adjusted in route index file

--- backend-server/routes/index.js
+++ backend-server/routes/index.js
@@ -5,12 +5,19 @@ const AppError = require('../utils/appError');
 
 // Importing the routers
 const userRouter = require('./userRoutes');
+const classroomRouter = require('./classroomRoutes');
+// const teacherRouter = require('./teacherRoutes');
+// const testRouter = require('./testRoutes');
 
 // Importing express router
 const router = require('express').Router();
 
 // Registering all routers
 router.use('/users', userRouter);
+router.use('/classrooms', classroomRouter)
+// router.use('/teachers', teacherRouter);
+// router.use('/classrooms', classr);
+// router.use('/test', testRouter);
 
 // The 404 route
 router.all('*', (req, res, next) => next(new AppError('Not found', 404)));
