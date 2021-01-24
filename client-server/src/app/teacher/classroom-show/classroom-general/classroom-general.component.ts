be809417c4c7b9f1daa2768a2f0f9f03b6c77a92
Souhardya Saha <43247008+SouhardyaSaha@users.noreply.github.com>
2021-01-24 17:14:03 +0600

    comments ui changed and made responsive

--- client-server/src/app/teacher/classroom-show/classroom-general/classroom-general.component.ts
+++ client-server/src/app/teacher/classroom-show/classroom-general/classroom-general.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-classroom-general',
   templateUrl: './classroom-general.component.html',
-  styleUrls: ['./classroom-general.component.css']
+  styleUrls: ['./classroom-general.component.css'],
 })
 export class ClassroomGeneralComponent implements OnInit {
+  panelOpenState = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
