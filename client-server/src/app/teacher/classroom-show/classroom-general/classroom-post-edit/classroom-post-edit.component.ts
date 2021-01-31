96776907a71992bb5b5ee2c0a50796d908a31ca0
SouhardyaSaha <souhardyasaha98@gmail.com>
2021-01-31 23:34:31 +0600

    Drag and drop added for posts

--- client-server/src/app/teacher/classroom-show/classroom-general/classroom-post-edit/classroom-post-edit.component.ts
+++ client-server/src/app/teacher/classroom-show/classroom-general/classroom-post-edit/classroom-post-edit.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FileSystemDirectoryEntry,
+  FileSystemFileEntry,
+  NgxFileDropEntry,
+  NgxFileDropModule,
+} from 'ngx-file-drop';
 
 @Component({
   selector: 'app-classroom-post-edit',
@@ -8,33 +14,6 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ClassroomPostEditComponent implements OnInit {
   classroomPostForm: FormGroup;
-
-  sessions = [
-    {
-      id: 1,
-      name: '2017-18',
-    },
-    {
-      id: 1,
-      name: '2016-17',
-    },
-    {
-      id: 1,
-      name: '2018-19',
-    },
-  ];
-
-  courses = [
-    {
-      id: 1,
-      title: 'Data Science',
-    },
-    {
-      id: 2,
-      title: 'Data Structure',
-    },
-  ];
-
   constructor() {}
 
   ngOnInit(): void {
@@ -51,5 +30,57 @@ export class ClassroomPostEditComponent implements OnInit {
 
   onSubmit() {
     console.log(this.classroomPostForm.value);
+  }
+
+  public files: NgxFileDropEntry[] = [];
+
+  public dropped(files: NgxFileDropEntry[]) {
+    // console.log(this.files);
+    // this.files.
+    this.files = this.files.concat(...files);
+    // this.files = [...this.files, ...files];
+    for (const droppedFile of files) {
+      // Is it a file?
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        fileEntry.file((file: File) => {
+          // Here you can access the real file
+          console.log(droppedFile.relativePath, file);
+
+          /**
+          // You could upload it like this:
+          const formData = new FormData()
+          formData.append('logo', file, relativePath)
+
+          // Headers
+          const headers = new HttpHeaders({
+            'security-token': 'mytoken'
+          })
+
+          this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
+          .subscribe(data => {
+            // Sanitized logo returned from backend
+          })
+          **/
+        });
+      } else {
+        // It was a directory (empty directories are added, otherwise only files)
+        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+        console.log(droppedFile.relativePath, fileEntry);
+      }
+    }
+  }
+
+  public fileOver(event) {
+    // console.log(event);
+  }
+
+  public fileLeave(event) {
+    // console.log(event);
+  }
+
+  public removeFile(file: NgxFileDropEntry) {
+    let index = this.files.indexOf(file);
+    if (index >= 0) this.files.splice(index, 1);
   }
 }
