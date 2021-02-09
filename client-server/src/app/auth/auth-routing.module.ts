e59d3ed5a641d8ec7e1891600f07e1831b038e47
SouhardyaSaha <souhardyasaha98@gmail.com>
2021-02-10 03:26:14 +0600

    code cleanup

--- client-server/src/app/auth/auth-routing.module.ts
+++ client-server/src/app/auth/auth-routing.module.ts
@@ -2,23 +2,29 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth.component';
 import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
+    component: AuthComponent,
     canActivate: [AuthGuard],
-    component: AuthComponent,
-    data: {
-      isRegisterMode: false,
-    },
-  },
-  {
-    path: 'register/:token/:role',
-    component: AuthComponent,
-    data: {
-      isRegisterMode: true,
-    },
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/auth/login',
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'register/:token/:role',
+        component: RegisterComponent,
+      },
+    ],
   },
 ];
 
--- client-server/src/app/auth/auth.component.ts
+++ client-server/src/app/auth/auth.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-
-import { AuthService } from './auth.service';
-import { Observable } from 'rxjs';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -11,12 +6,6 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
-  isRegistrationMode: boolean;
-  constructor(
-    private route: ActivatedRoute,
-    private authService: AuthService,
-  ) {}
-  ngOnInit(): void {
-    this.isRegistrationMode = this.route.snapshot.data['isRegisterMode'];
-  }
+  constructor() {}
+  ngOnInit(): void {}
 }
